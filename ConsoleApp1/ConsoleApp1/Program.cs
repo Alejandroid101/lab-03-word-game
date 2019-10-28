53357a00334bf3cf5381469c99a39697ce795241
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-27 21:21:48 -0700

    Great progress, but forgot to ACP earlier. I thing I only need to finish the StartGame MEthod and im done!

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to the Guessing game!");
+                string[] words = FileSetup();
+                UserInterface(words);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.Write("Play again soon!");
+            }
+        }
+        public static void UserInterface(string[] words)
+        {
+            Console.WriteLine("1.)View all available words.");
+            Console.WriteLine("2.)Add a word to the list.");
+            Console.WriteLine("3.)Remove a word from the list.");
+            Console.WriteLine("4.)PLAY THE GAME!");
+            Console.WriteLine("5.)Exit");
+
+            int userChoice;
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                userChoice = Convert.ToInt32(input);
+                if (userChoice >= 1 && userChoice <= 5)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option");
+                }
+            }
+
+            switch(userChoice)
+            {
+                case 1:
+                    ViewWords(words);
+                    break;
+                case 2:
+                    AddWord();
+                    break;
+                case 3:
+                    RemoveWord();
+                    break;
+                case 4:
+                    StartGame(words);
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+
+
+        }
+
+        //Done
+        static string[] FileSetup()
+        {
+            string filePath = "../../../wordsFile.txt";
+            string[] starterFile = new string[1] { "papaya" };
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    for (int i = 0; i < starterFile.Length; i++)
+                    {
+                        sw.WriteLine(starterFile[i]);
+                    }
+                }
+            }
+            string[] availableWords = File.ReadAllLines(filePath);
+            return availableWords;
+
+        }
+
+        public static void ViewWords(string[] words)
+        {
+            Console.WriteLine("View Words!");
+            string filePath = "../../../wordsFile.txt";
+            using (StreamReader stringreader = File.OpenText(filePath))
+            {
+                string readingWords;
+                while ((readingWords = stringreader.ReadLine()) != null)
+                {
+                    Console.WriteLine(readingWords);
+                }
+            }
+            UserInterface(words);
+        }
+
+        public static void RemoveWord()
+        {
+            Console.WriteLine("Remove words!");
+            string filePath = "../../../wordsFile.txt";
+            using (StreamReader stringreader = File.OpenText(filePath))
+            {
+                string readingWords;
+                while ((readingWords = stringreader.ReadLine()) != null)
+                {
+                    Console.WriteLine(readingWords);
+                }
+            }
+
+            while(true)
+            {
+                Console.WriteLine("Write 'done' to finish removing words");
+                Console.Write("Enter word to remove:");
+
+                string remove = Console.ReadLine();
+
+                if(remove == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] oldList = File.ReadAllLines(filePath);
+                    string[] newList = new string[oldList.Length - 1];
+
+                    int j = 0;
+                    for (int i = 0; i < oldList.Length; i++)
+                    {
+                        if (remove != oldList[i]){
+                            newList[j] = oldList[i];
+                            j++;
+                        }
+                    }
+                    File.Delete(filePath);
+
+                    using (StreamWriter sw = File.CreateText(filePath))
+                    {
+                        for (int i = 0; i < newList.Length; i++)
+                        {
+                            sw.WriteLine(newList[i]);
+                        }
+                    }
+
+                    Console.WriteLine("Updated list!");
+                    using(StreamReader sr = File.OpenText(filePath))
+                    {
+                        string s;
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                }
+            }
+            string[] words = FileSetup();
+            UserInterface(words);
+        }
+        public static void AddWord()
+        {
+            Console.WriteLine("Add word!");
+
+            string filePath = "../../../wordsFile.txt";
+            using (StreamReader stringreader = File.OpenText(filePath))
+            {
+                string readingWords;
+                while ((readingWords = stringreader.ReadLine()) != null)
+                {
+                    Console.WriteLine(readingWords);
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Write 'done' to finish removing words");
+                Console.Write("Enter word to remove:");
+
+                string addWord = Console.ReadLine();
+
+                if (addWord == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(filePath))
+                    {
+                        sw.WriteLine(addWord);
+                    }
+
+                    Console.WriteLine();
+                }
+
+            }
+        }
+        public static void StartGame(string[] words)
+        {
+            Console.WriteLine("Guess the word!!");
+
+            Random rdmWord = new Random();
+            int rdmIndex = rdmWord.Next(0, words.Length - 1);
+            string guessing = words[rdmIndex];
+
+
+
+        }
+    }
+}
